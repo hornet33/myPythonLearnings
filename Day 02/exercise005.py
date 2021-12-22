39e3e9886d5690a602a9601ded64f50c016de792
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-22 13:33:27 +0530

    Day 2 exercises

---
+++ Day 02/exercise005.py
@@ -0,0 +1,3 @@
+# Python program to take a 2 digit number input and print the sum of those 2 digits
+two_digit_number = input("Type a two digit number: ")
+print(int(two_digit_number[0])+int(two_digit_number[1]))
