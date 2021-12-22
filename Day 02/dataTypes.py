8b2d7551ef156f107f6e52f2c798a3a6279ec3d3
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-22 11:45:06 +0530

    Day 2 exercises

---
+++ Day 02/dataTypes.py
@@ -0,0 +1,24 @@
+# Strings and subscripts
+s = "Hello"  # This is a string
+c = s[0]  # Will assign 'H' to variable 'c' - this is called a subscript in Python
+
+print(s + " " + c)
+
+# Integers
+i = 123
+print(i)
+i = 123_123_123  # In Python we can give underscore to separate number places like thousands, millions, etc for better readability
+print(i)
+
+# Floats - numbers with decimals
+f = 123.34
+print(f)
+f = 123_123_349.25
+print(f)
+
+# Booleans - True or False (case-sensitive - cannot be 'true' or 'TRUE', must be only 'True' or 'False')
+b = True
+print(b)
+# b = false -- this statement will give an error
+b = False
+print(b)
