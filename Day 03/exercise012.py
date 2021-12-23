117536274a04af1bfebdfaac4d5daa75abb20f03
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-23 10:04:54 +0530

    Day 3 exercises

---
+++ Day 03/exercise012.py
@@ -0,0 +1,33 @@
+# Python program for "Love Calculator" :-)
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+# Converting names to upper case
+upper_name1 = name1.upper()
+upper_name2 = name2.upper()
+
+# Checking occurrence of "TRUE"
+count_T = upper_name1.count("T") + upper_name2.count("T")
+count_R = upper_name1.count("R") + upper_name2.count("R")
+count_U = upper_name1.count("U") + upper_name2.count("U")
+count_E = upper_name1.count("E") + upper_name2.count("E")
+count_TRUE = count_T + count_R + count_U + count_E
+
+# Checking occurrence of "LOVE"
+count_L = upper_name1.count("L") + upper_name2.count("L")
+count_O = upper_name1.count("O") + upper_name2.count("O")
+count_V = upper_name1.count("V") + upper_name2.count("V")
+# count_E = upper_name1.count("E") + upper_name2.count("E") -- Not needed since count of E is already done in TRUE
+count_LOVE = count_L + count_O + count_V + count_E
+
+# Calculate love percent
+love_percent = count_TRUE * 10 + count_LOVE
+
+# Print message according to love percent
+if love_percent < 10 or love_percent > 90:
+    print(f"Your score is {love_percent}, you go together like coke and mentos.")
+elif love_percent > 40 and love_percent < 50:
+    print(f"Your score is {love_percent}, you are alright together.")
+else:
+    print(f"Your score is {love_percent}.")
--- Day 03/logicalOperators.py
+++ Day 03/logicalOperators.py
@@ -1,12 +1,18 @@
-# Python conditional operators and control flow with if / else statements
-# Indentation and spacing is crucial in Python - no braces like Java, an if/else block is recognized by the indentation
+# Python uses 'and' , 'or', 'not' as logical operators
+# a = 12
+# b = 15
+# print((a > 14) and (b > 20)) will print False
+
+# The ticketing program from conditionalStatements.py using logical operators
 i = int(input("Enter your height in cm: "))
 bill = 0
 if i >= 120:
     a = int(input("Enter your age in years: "))
 
     # # # Nested Conditions
-    if a >= 18:
+    if a >= 45 and a <= 55:
+        print("Your ticket is free!")
+    elif a >= 18:
         bill = 12
         print("One adult ticket is $12")
     elif a >= 12:  # Else if
@@ -25,5 +31,3 @@ if i >= 120:
     print(f"Your total bill is ${bill}")
 else:
     print("Sorry, you cannot ride the roller-coaster!")
-
-# Logical operators in logicalOperators.py program
