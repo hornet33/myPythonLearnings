ef0024f6ea016c30845f194413ee07f855ef2950
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-23 10:26:49 +0530

    Day 2 exercises

--- Day 02/assignment002.py
+++ Day 02/assignment002.py
@@ -8,5 +8,6 @@ bill_value = float(input("Enter the total bill: $"))
 tip_percent = int(input("Enter the desired service tip percentage (10, 12 or 15): "))
 num_of_people = int(input("Enter the number of people: "))
 per_person_amount = "{:.2f}".format((bill_value / num_of_people) * (1 + (tip_percent / 100)))
+# '{:.2f}' is for formatting the output to always 2 decimal places - so "33.6" will display as "33.60"
 fMessage = f"Each person should pay ${per_person_amount}"
 print(fMessage)
