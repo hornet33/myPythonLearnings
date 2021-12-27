d32072f3097ff3192100c8e75d946181c49bd765
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-27 12:53:31 +0530

    Day 5 exercises

--- Day 05/assignment05.py
+++ Day 05/assignment05.py
@@ -12,6 +12,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Variable initialization
 random_letters = ""
 random_symbols = ""
 random_numbers = ""
