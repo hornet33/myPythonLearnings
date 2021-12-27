985837664d3be446c818527e060465ffd2bdddc7
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-27 15:25:18 +0530

    Day 6 exercises

---
+++ Day 06/userDefinedFunctions.py
@@ -0,0 +1,7 @@
+# Start with "def" keyword, function name followed by argument list, ended with a colon
+def new_function():
+    print("This is a new function")
+
+
+# Invoking the user defined function
+new_function()
---
+++ Day 06/whileLoops.py
@@ -0,0 +1,5 @@
+# Sample use of while loop in Python below:
+i = 10
+while i > 0:
+    print(i)
+    i -= 1
