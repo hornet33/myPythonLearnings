046e084dee7a604f21700e7e560f8e9ed089eb14
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-28 19:31:04 +0530

    Day 9 exercises

--- Day 09/dictionaries.py
+++ Day 09/dictionaries.py
@@ -30,3 +30,73 @@ new_dictionary = {}
 # Clear a dictionary
 my_car_dictionary = {}  # Wipes out the dictionary and sets it to an empty dictionary
 print(my_car_dictionary)
+
+# ---- Nesting lists and dictionaries
+
+# Nesting a list within a dictionary
+travel_log = {
+    "Karnataka": ["Mysore", "Bangalore", "Udupi", "Mangalore", "Coorg"],
+    "Kerala": ["Wayanad", "Bekal", "Kottayam"],
+    "Bengal": ["Siliguri", "Kolkata", "Darjeeling", "Kalimpong"],
+    "Maharashtra": ["Mumbai", "Pune"],
+    "Tamil Nadu": ["Chennai", "Ooty", "Kodaikanal", "Yercaud"],
+}
+
+# Nesting dictionaries and lists within a dictionary
+detail_travel_log = {
+    "Karnataka": {
+        "cities_visited": ["Mysore", "Bangalore", "Udupi", "Mangalore", "Coorg"],
+        "years_visited": [2016, 2021, 2015, 2015, 2017],
+    },
+    "Kerala": {
+        "cities_visited": ["Wayanad", "Bekal", "Kottayam"],
+        "years_visited": [2011, 2011, 2014],
+    },
+    "Bengal": {
+        "cities_visited": ["Siliguri", "Kolkata", "Darjeeling", "Kalimpong"],
+        "years_visited": [2021, 2018, 2021, 2020],
+    },
+    "Maharashtra": {
+        "cities_visited": ["Mumbai", "Pune"],
+        "years_visited": [2006, 2007],
+    },
+    "Tamil Nadu": {
+        "cities_visited": ["Chennai", "Ooty", "Kodaikanal", "Yercaud"],
+        "years_visited": [2010, 2016, 2017, 2014],
+    },
+}
+
+# Accessing elements of nested dictionaries and lists within a dictionary by the key element
+print(detail_travel_log.get("Karnataka").get("cities_visited")[0])
+
+# Nesting dictionaries and lists within a list
+list_travel_log = [
+    {
+        "state": "Karnataka",
+        "cities_visited": ["Mysore", "Bangalore", "Udupi", "Mangalore", "Coorg"],
+        "years_visited": [2016, 2021, 2015, 2015, 2017],
+    },
+    {
+        "state": "Kerala",
+        "cities_visited": ["Wayanad", "Bekal", "Kottayam"],
+        "years_visited": [2011, 2011, 2014],
+    },
+    {
+        "state": "Bengal",
+        "cities_visited": ["Siliguri", "Kolkata", "Darjeeling", "Kalimpong"],
+        "years_visited": [2021, 2018, 2021, 2020],
+    },
+    {
+        "state": "Maharashtra",
+        "cities_visited": ["Mumbai", "Pune"],
+        "years_visited": [2006, 2007],
+    },
+    {
+        "state": "Tamil Nadu",
+        "cities_visited": ["Chennai", "Ooty", "Kodaikanal", "Yercaud"],
+        "years_visited": [2010, 2016, 2017, 2014],
+    },
+]
+
+# Accessing elements in dictionaries nested within a list is by list item position
+print(list_travel_log[0].get("cities_visited")[0])
---
+++ Day 09/exercise024.py
@@ -0,0 +1,33 @@
+# Python program to add a new dictionary (with nested list) which is nested inside an outer list
+
+# Original list with nested inner dictionary with a nested list
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+
+
+# Function that will add a new list item (dictionary) in the travel_log list
+def add_new_country(country, visits, city_list):
+    position = len(travel_log)  # Get the last position of the list - new item will go here
+    travel_log.append({})  # Append an empty dictionary to the outer list
+
+    # Add the items in the nested dictionary within the outer list
+    travel_log[position]["country"] = country
+    travel_log[position]["visits"] = visits
+    travel_log[position]["cities"] = city_list
+
+
+# Function call to add values
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+
+# Print the final list after insertion
+print(travel_log)
