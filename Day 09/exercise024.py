48687ca16706a03c60f71e037b9551d69a8ae71f
rahul_pradhan@outlook.com <rahul_pradhan@outlook.com>
2021-12-29 09:47:17 +0530

    Day 9 exercises

--- Day 09/exercise024.py
+++ Day 09/exercise024.py
@@ -17,6 +17,7 @@ travel_log = [
 
 # Function that will add a new list item (dictionary) in the travel_log list
 def add_new_country(country, visits, city_list):
+    # --- Approach 1
     position = len(travel_log)  # Get the last position of the list - new item will go here
     travel_log.append({})  # Append an empty dictionary to the outer list
 
@@ -24,6 +25,14 @@ def add_new_country(country, visits, city_list):
     travel_log[position]["country"] = country
     travel_log[position]["visits"] = visits
     travel_log[position]["cities"] = city_list
+
+    # --- Approach 2
+    new_entry = {
+        "country": country,
+        "visits": visits,
+        "cities": city_list
+    }
+    travel_log.append(new_entry)
 
 
 # Function call to add values
